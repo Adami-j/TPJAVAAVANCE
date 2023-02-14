aabd84d9ce4821c8944658f3e502a9c279d088ea
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-14 14:18:50 +0100

    Creation classe TestFizzBuzz et FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,6 @@
+public class FizzBuzz {
+
+    public static String fizzBuzz(int i) {
+        return "1";
+    }
+}
---
+++ src/TestFizzBuzz.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static  org.junit.Assert.assertEquals;
+
+public class TestFizzBuzz {
+
+    @Test
+    public void testFizzBuzz(){
+        assertEquals("1",FizzBuzz.fizzBuzz(1));
+    }
+    
+    
+
+
+}
