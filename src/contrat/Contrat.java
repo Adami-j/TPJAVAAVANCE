0e2a5217f3e361f4be5e3c10bbfeb36b3b85265f
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 11:14:15 +0100

    contrat EX1

---
+++ src/contrat/Contrat.java
@@ -0,0 +1,18 @@
+package contrat;
+
+import java.util.Set;
+
+public class Contrat <T>{
+    Set<T> insersect(Set<T> A, Set<T> B){
+
+        return null;
+    }
+    // il faut que le les deux paramètres que fournit le clientne soient pas null, aient des éléments qui ont le type T
+
+
+    int intretirerArgent(int intmontant){
+        return 0;
+    }
+    // contrat, le montant ne doit pas être null, ni négatif, ni un autre paramètre d'un type différent de int
+    //retourner
+}
