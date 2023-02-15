241f11ee47ab18fe00c9cfc2b248ca4b96fc725e
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 11:00:23 +0100

    sortie =3

--- src/NombreRomain.java
+++ src/NombreRomain.java
@@ -1,10 +1,16 @@
 public class NombreRomain {
     public static String splitNombreRomain(int i) {
         int y =1;
+        String res = "";
         if(i==0){
             return "";
         }
-        return "I";
+
+        while(y<=i){
+            res+="I";
+            y++;
+        }
+        return res;
     }
 
 }
