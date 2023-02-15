603d05ee1557802806b802733c9304702ffe006e
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 09:11:48 +0100

    ajout fonction isUppercase pour vérifier si le nom en entrée est en majuscule

--- src/BonjourNom.java
+++ src/BonjourNom.java
@@ -2,12 +2,25 @@ public class BonjourNom {
 
     public static String bonjourNom(String nom){
         String res = "";
+
         if(nom!=null && !nom.equals("")){
-            res=nom;
+               res=nom;
         }else{
             res="ami";
         }
+        if(isUppercase(nom)){
+            return "BONJOUR, "+res;
+        }
         return "Bonjour, "+res;
+    }
+
+    public static Boolean isUppercase(String nom){
+        for (Character c : nom.toCharArray()){
+            if (!Character.isUpperCase(c)){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
