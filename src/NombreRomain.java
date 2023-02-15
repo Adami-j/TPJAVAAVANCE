1366bb024f34123ee65b565b49f7223eca3c03c3
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 10:57:41 +0100

    gestion du cas i == 0

--- src/NombreRomain.java
+++ src/NombreRomain.java
@@ -1,5 +1,10 @@
 public class NombreRomain {
     public static String splitNombreRomain(int i) {
+        int y =1;
+        if(i==0){
+            return "";
+        }
         return "I";
     }
+
 }
