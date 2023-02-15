5b2891b4debd07e67fff03138176bc19790be53b
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 09:38:51 +0100

    Reprise des tests avec une arraylist

--- src/TestBonjourNom.java
+++ src/TestBonjourNom.java
@@ -1,29 +1,55 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class TestBonjourNom {
 
+    private ArrayList<String> listeDenoms = new ArrayList<>();
+
+    @Before
+    public void setUp(){
+
+    }
+
     @Test
     public void bonjourNomNom(){
-        assertEquals("Bonjour, Ju",BonjourNom.bonjourNom("Ju"));
+        listeDenoms.add("Ju");
+        assertEquals("Bonjour, Ju",BonjourNom.bonjourNom(listeDenoms));
     }
 
     @Test
     public void bonjourNull(){
+        listeDenoms.add(null);
         assertEquals("Bonjour, ami",BonjourNom.bonjourNom(null));
     }
 
     @Test
     public void bonjourVide(){
-        assertEquals("Bonjour, ami",BonjourNom.bonjourNom(""));
+        listeDenoms.add("");
+        assertEquals("Bonjour, ami",BonjourNom.bonjourNom(listeDenoms));
     }
 
     @Test
     public void bonjourHurler(){
-        assertEquals("BONJOUR, NOUPY",BonjourNom.bonjourNom("NOUPY"));
+        listeDenoms.add("NOUPY");
+        assertEquals("BONJOUR, NOUPY",BonjourNom.bonjourNom(listeDenoms));
     }
 
+    @Test
+    public void bonjourDemiHurle(){
+        listeDenoms.add("Malik");
+        assertEquals("Bonjour, Malik",BonjourNom.bonjourNom(listeDenoms));
+    }
+
+    @Test
+    public void bonjourPlusieursNoms(){
+        listeDenoms.add("Tic");
+        listeDenoms.add("Tac");
+        assertEquals("Bonjour, Tic et Tac", BonjourNom.bonjourNom(new ArrayList<String>()));
+    }
 
 }
