bac80adb96eae825353d24bef603211b6fab7419
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-15 08:55:27 +0100

    ajout condition vide dans la méthode bonjourNom

--- src/BonjourNom.java
+++ src/BonjourNom.java
@@ -2,15 +2,12 @@ public class BonjourNom {
 
     public static String bonjourNom(String nom){
         String res = "";
-        if(nom!=null){
+        if(nom!=null && !nom.equals("")){
             res=nom;
         }else{
             res="ami";
         }
-
         return "Bonjour, "+res;
-
-
     }
 
 }
