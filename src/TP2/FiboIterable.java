11ca20b9a13fe4b680202dee2777b34de6580765
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-20 09:32:16 +0100

    TP2 ex2 ajout classe FiboIterable avec le psvm main pour afficher les 50 éléments fibo

---
+++ src/TP2/FiboIterable.java
@@ -0,0 +1,24 @@
+package TP2;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+
+public class FiboIterable implements Iterable<Integer> {
+    private int i;
+
+    public FiboIterable(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new FiboComparateur(i);
+    }
+
+    public static void main(String[] args) {
+        FiboIterable fibo = new FiboIterable(50);
+        for (int i : fibo) {
+            System.out.print(i + " ");
+        }
+    }
+}
