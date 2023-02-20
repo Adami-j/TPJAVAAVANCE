b9d857691748059061c239f5ecf5ad68f5809fc8
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-20 09:25:04 +0100

    TP2 ex2 fonctionnement et test de next() fibonacci

--- src/TP2/FiboComparateur.java
+++ src/TP2/FiboComparateur.java
@@ -3,22 +3,36 @@ package TP2;
 import java.util.Iterator;
 
 public class FiboComparateur implements Iterator<Integer> {
-    private int debut = 0;
+
     private int fin = 1;
     private int i = 0;
+    private int debut = i;
 
     public FiboComparateur(int i) {
         this.i = i;
+
     }
 
     @Override
     public boolean hasNext() {
-        return debut!=fin?true:false;
+        if(fin==1 && debut==1){
+            return true;
+        }
+        return debut+fin<=i?true:false;
     }
 
     @Override
     public Integer next() {
-        return ;
+        int resultat = debut+fin;
+        debut = fin;
+        fin= resultat;
+        return resultat;
     }
 
+    public static void main(String[] args) {
+        FiboComparateur fi = new FiboComparateur(15);
+        while (fi.hasNext()){
+            System.out.println(fi.next());
+        }
+    }
 }
